9fe2ea7117b343be5f313271e5037ce14e690c0c
Anton Yefremov <ayefremov@spscommerce.com>
2015-10-16 20:44:29 +0300

    watch, interpolate

--- js/app.js
+++ js/app.js
@@ -1,24 +1,66 @@
-var app = angular.module('myApp', []);
+angular.module('emailParser', [])
+    //.config(['$interpolateProvider',
+    //    function($interpolateProvider) {
+    //        $interpolateProvider.startSymbol('__');
+    //        $interpolateProvider.endSymbol('__');
+    //    }
+    //])
+    .factory('EmailParser', ['$interpolate',
+        function($interpolate) {
+            return {
+                parse: function(text, context) {
+                    var template = $interpolate(text);
+                    return template(context);
+                }
+            }
+        }
+    ]);
 
-app.controller('MyController', function($scope) {
-    $scope.name = "Ari";
+var myApp = angular.module('myApp', ['emailParser']);
 
-    $scope.person = {
-        name: "Anton Yefremov"
-    };
-});
+myApp.controller('MyController',
+    ['$scope', 'EmailParser',
+        function($scope, EmailParser) {
+            $scope.name = "Ari";
 
-app.controller('FirstController', function($scope) {
+            $scope.person = {
+                name: "Anton Yefremov"
+            };
+
+            $scope.$watch('expr', function(newVal, oldVal, scope) {
+                if (newVal !== oldVal) {
+                    var parseFun = $parse(newVal);
+                    $scope.parsedValue = parseFun(scope);
+                    console.log(oldVal, newVal, scope);
+                }
+            });
+
+            $scope.to = 'ari@fullstack.io';
+            $scope.emailBody = 'Hello {{ to }}';
+             //Set up a watch
+            $scope.$watch('emailBody', function(body) {
+                if (body) {
+                    $scope.previewText =
+                        EmailParser.parse(body, {
+                            to: $scope.to
+                        });
+                }
+            });
+
+        }
+    ]);
+
+myApp.controller('FirstController', function($scope) {
     $scope.counter = 0;
     $scope.add = function(amount) {$scope.counter += amount;};
     $scope.subtract = function(amount) {$scope.counter -= amount;};
 });
 
-app.controller('ParentController', function($scope) {
+myApp.controller('ParentController', function($scope) {
     $scope.person = {greeted: false};
 });
 
-app.controller('ChildController', function($scope) {
+myApp.controller('ChildController', function($scope) {
     $scope.sayHello = function() {
         $scope.person.name = "Eddie Murphy";
         $scope.person.greeted = true;
