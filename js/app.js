85082b4e63efae9be874c32134d774560ceefa0c
Anton Yefremov <ayefremov@spscommerce.com>
2015-10-19 20:15:32 +0300

    show error messages on input blur instead of showing them on submit

--- js/app.js
+++ js/app.js
@@ -104,6 +104,28 @@ myApp.directive('ensureUnique', function($http) {
     };
 });
 
+myApp.directive('ngFocus', [function() {
+    var FOCUS_CLASS = "ng-focused";
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            ctrl.$focused = false;
+            element.bind('focus', function(event) {
+                element.addClass(FOCUS_CLASS);
+                scope.$apply(function() {
+                    ctrl.$focused = true;
+                });
+            }).bind('blur', function(event) {
+                element.removeClass(FOCUS_CLASS);
+                scope.$apply(function() {
+                    ctrl.$focused = false;
+                });
+            });
+        }
+    };
+}]);
+
 myApp.directive('oneToTen', function() {
     return {
         require: '?ngModel',
