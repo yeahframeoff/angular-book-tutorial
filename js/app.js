38d54703fbeec7bf8eca96a787cd44a5948afabe
Anton Yefremov <ayefremov@spscommerce.com>
2015-10-19 19:02:21 +0300

    fiklters, directives, validation

--- js/app.js
+++ js/app.js
@@ -66,3 +66,71 @@ myApp.controller('ChildController', function($scope) {
         $scope.person.greeted = true;
     };
 });
+
+myApp.controller('DemoController', ['$scope', '$filter',
+    function($scope, $filter) {
+        $scope.name = $filter('lowercase')('Ari');
+
+        $scope.isCapitalized = function(str) {
+            return str[0] == str[0].toUpperCase();
+        }
+}]);
+
+myApp.filter('capitalize', function() {
+        return function(input) {
+            if (input)
+                return input[0].toUpperCase() + input.slice(1);
+        }
+    }
+);
+
+myApp.directive('ensureUnique', function($http) {
+    return {
+        require: 'ngModel',
+        link: function(scope, ele, attrs, c) {
+            scope.$watch(attrs.ngModel, function(n) {
+                if (!n) return;
+                $http({
+                    method: 'POST',
+                    url: '/api/check/' + attrs.ensureUnique,
+                    data: {'field': attrs.ensureUnique}
+                }).success(function(data, status, headers, cfg) {
+                    c.$setValidity('unique', data.isUnique);
+                }).error(function(data, status, headers, cfg) {
+                    c.$setValidity('unique', false);
+                });
+            })
+        }
+    };
+});
+
+myApp.directive('oneToTen', function() {
+    return {
+        require: '?ngModel',
+        link: function(scope, ele, attrs, ngModel) {
+            if (!ngModel) return;
+            ngModel.$parsers.unshift(
+                function(viewValue) {
+                    var i = parseInt(viewValue);
+
+                    if (i >= 0 && i < 10) {
+                        ngModel.$setValidity('oneToTen', true);
+                        return viewValue;
+                    }
+                    else {
+                        ngModel.$setValidity('oneToTen', false);
+                        return undefined;
+                    }
+                }
+            )
+        }
+    }
+});
+
+myApp.controller('SignupController', function($scope) {
+    $scope.submitted = false;
+    $scope.signupForm = function() {
+        if ($scope.signup_form.$invalid)
+            $scope.signup_form.submitted = true;
+    }
+});
